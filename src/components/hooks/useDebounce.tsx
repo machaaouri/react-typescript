6853f4ce8b58725793d20a2d51ecff94addad48f
hmachaao <hmachaao@gmail.com>
2019-08-15 23:19:45 +0200

    Hook to debounce fast changing value

--- src/components/hooks/useDebounce.tsx
+++ src/components/hooks/useDebounce.tsx
@@ -1,9 +1,77 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 
 export const Countries = () => {
 
-    console.log(searchCountryByName('Den'));
-    return <div>Countries</div>
+  // Search country name
+  const [name, setName] = useState('');
+  // API search results
+  const [results, setResults] = useState<Country[]>([]);
+  // Searching status (whether there is pending API request)
+  const [isSearching, setIsSearching] = useState(false);
+
+  // Debounce search name so that it only gives us latest value ...
+  // if name has not been updated within last 500ms.
+  // The goal is to only have the API call fire when user stops typing ...
+  // so that we aren't hitting our API rapidly.
+  const debouncedSearchName = useDebounce(name, 500);
+
+  useEffect(
+    () => {
+      if (debouncedSearchName) {
+        setIsSearching(true);
+        searchCountryByName(debouncedSearchName).then(results => {
+          setIsSearching(false);
+          setResults(results);
+        });
+      } else {
+        setResults([]);
+      }
+    },
+    [debouncedSearchName] // Only call effect if debounced search term changes
+  );
+
+
+  return (
+    <div>
+      <input
+        placeholder="Search Country by name"
+        onChange={e => setName(e.target.value)}
+      />
+  
+      {isSearching && <div>Searching ...</div>}
+
+      {results.map(result => (
+        <div key={result.alpha2Code}>
+          <h4>{result.name}</h4>
+          <img src={result.flag} width="20" height="20" />
+        </div>
+      ))}
+    </div>
+  );
+
+}
+
+
+const useDebounce = (value: string, delay: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(
+        () => 
+            {
+                const handler = setTimeout(() => {
+                    setDebouncedValue(value);
+                }, delay);
+
+            // Cancel the timeout if value changes
+            // This is how we prevent debounced value from updating if value is changed ...
+            // .. within the delay period. Timeout gets cleared and restarted.
+            return () => {
+                clearTimeout(handler);
+            };
+            }, [value, delay] // Only re-call effect if value or delay changes
+    )
+    return debouncedValue;
 }
 
 // 
